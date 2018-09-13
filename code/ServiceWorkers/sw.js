f1346329759b805a0a20e4549b0a9992840bd07c
homobulla <homobulla@outlook.com>
2018-09-13 10:22:02 +0800

    feat: add test ServiceWorkers

---
+++ code/ServiceWorkers/sw.js
@@ -0,0 +1,19 @@
+if ('serviceWorker' in navigator) {
+    //环境特性检查
+    console.log('zhichi ')
+    navigator.serviceWorker
+        .register('/Technical-tree/ServiceWorkers/sw.js', {
+            scope: '/Technical-tree/ServiceWorkers/'
+        })
+        // scope可使用的域范围,即service worker 控制的内容的子目录
+        .then(function(reg) {
+            // registration worked
+            console.log('Registration succeeded. Scope is ' + reg.scope)
+        })
+        .catch(function(error) {
+            // registration failed
+            console.log('Registration failed with ' + error)
+        })
+} else {
+    console.log('不支持sw')
+}
