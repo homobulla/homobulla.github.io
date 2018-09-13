b65bfb5e7981e8cbaa7e4c79271834b742352b77
homobulla <homobulla@outlook.com>
2018-09-13 11:04:00 +0800

    feat:

---
+++ code/ServiceWorkers/app.js
@@ -0,0 +1,19 @@
+if ('serviceWorker' in navigator) {
+    //环境特性检查
+    console.log('zhichi ')
+    navigator.serviceWorker
+        .register('/Technical-tree/code/ServiceWorkers/sw.js', {
+            scope: '/Technical-tree/code/ServiceWorkers/'
+        })
+        // scope可使用的域范围,即service worker 控制的内容的子目录
+        .then(function(reg) {
+            // registration worked
+            console.log('Registration succeeded. Scope is ' + reg.scope)
+        })
+        .catch(function(error) {
+            // registration failed
+            console.log('Registration failed with ' + error)
+        })
+} else {
+    alert('不支持sw')
+}
--- code/ServiceWorkers/sw.js
+++ code/ServiceWorkers/sw.js
@@ -1,19 +1,27 @@
-if ('serviceWorker' in navigator) {
-    //环境特性检查
-    console.log('zhichi ')
-    navigator.serviceWorker
-        .register('/Technical-tree/code/ServiceWorkers/sw.js', {
-            scope: '/Technical-tree/code/ServiceWorkers/'
+// this.addEventListener('install', function(event) {
+//     event.waitUntil(
+//         caches.open('v1').then(function(cache) {
+//             return cache.addAll([
+//                 '/Technical-tree/code/',
+//                 '/Technical-tree/code/index.html',
+//                 '/Technical-tree/code/app.js',
+//                 '/Technical-tree/code/timg.jpg'
+//             ])
+//         })
+//     )
+// })
+this.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('v1').then(function(cache) {
+            return cache.addAll([
+                '/Technical-tree/code/',
+                '/Technical-tree/code/index.html',
+                '/Technical-tree/code/app.js',
+                '/Technical-tree/code/timg.jpg'
+            ])
         })
-        // scope可使用的域范围,即service worker 控制的内容的子目录
-        .then(function(reg) {
-            // registration worked
-            console.log('Registration succeeded. Scope is ' + reg.scope)
-        })
-        .catch(function(error) {
-            // registration failed
-            console.log('Registration failed with ' + error)
-        })
-} else {
-    console.log('不支持sw')
-}
+    )
+})
+this.addEventListener('fetch', function(event) {
+    event.respondWith(caches.match(event.request))
+})
